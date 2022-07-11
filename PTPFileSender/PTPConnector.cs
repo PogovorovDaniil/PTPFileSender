cc186294d4e35963b39d089e4225bf8e157459d2
Daniil Pogovorov <GigSter2000@yandex.ru>
2022-07-12 00:26:04 +0300

    Init

---
+++ PTPFileSender/PTPConnector.cs
@@ -0,0 +1,18 @@
+﻿using GPeerToPeer.Client;
+
+namespace PTPFileSender
+{
+    public static class PTPConnector
+    {
+        private static PTPClient client = new PTPClient("193.61.3.168", 22345, 22345);
+        static PTPConnector()
+        {
+            client.ReceiveMessageFrom += Client_ReceiveMessageFrom;
+        }
+
+        private static void Client_ReceiveMessageFrom(byte[] message, PTPNode node)
+        {
+
+        }
+    }
+}
